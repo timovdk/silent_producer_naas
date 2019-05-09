2ea27893e4a897e17b16a72601f2fc8801824930
Erik Vullings <erik.vullings@gmail.com>
2019-05-09 18:05:09 +0200

    Working example of a producer and consumer in action

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # example-node-test-bed-adapter
-Example project for the node-test-bed-adapter
+
+Example project for the node-test-bed-adapter.
+
+The example assumes that the DRIVER-EU Apache Kafka-based test-bed is running. If not, see the [test-bed installation instructions](https://github.com/DRIVER-EU/test-bed) for installing a local version of the test-bed. 
+
+The example performs the following actions:
+
+- Installing the required `npm` packages, including the `node-test-bed-adapter`.
+- `producer.ts`: Upload the AVRO schemas from the `src/schemas` folder to the [Kafka schema registry](http://localhost:3601) (note that this is not required during production, when the test-bed admin tool takes over this role, so use it only once after starting the Docker-based test-bed to register all schemas). You can turn it off by settings `autoRegisterSchemas: false`.
+- `producer.ts`: Send 4 CAP messages to the test-bed. You can inspect them by visiting the [Kafka topics UI](http://localhost:3600).
+- `consumer.ts`: Receive CAP messages and log them to screen.
+
+## Installation
+
+```bash
+npm i
+npm run build
+```
+
+## Usage
+
+You can start them in two terminals, or one after the other.
+
+```bash
+npm run producer # To produce some CAP messages. Use CTRL-C to stop it.
+npm run consumer # To consume these CAP messages. Use CTRL-C to stop it.
+```
+
+## Develop
+
+```bash
+npm start
+```
---
+++ src/producer.ts
@@ -0,0 +1,140 @@
+import * as path from 'path';
+import { FeatureCollection } from 'geojson';
+import {
+  TestBedAdapter,
+  Logger,
+  LogLevel,
+  ProduceRequest,
+  largeFileUploadCallback,
+  DataType,
+  ITiming,
+  TimeState,
+  TimeTopic,
+} from 'node-test-bed-adapter';
+import * as amberAlert from './data/example_amber_alert.json';
+import * as earthquakeAlert from './data/example_earthquake.json';
+import * as thunderstormAlert from './data/example_thunderstorm.json';
+import * as homelandSecurityAlert from './data/example_homeland_security.json';
+
+const log = Logger.instance;
+
+class Producer {
+  private id = 'tno';
+  private adapter: TestBedAdapter;
+
+  constructor() {
+    this.adapter = new TestBedAdapter({
+      kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
+      schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
+      // largeFileService: 'localhost:9090',
+      // sslOptions: {
+      //   pfx: fs.readFileSync('../certs/other-tool-1-client.p12'),
+      //   passphrase: 'changeit',
+      //   ca: fs.readFileSync('../certs/test-ca.pem'),
+      //   rejectUnauthorized: true,
+      // },
+      clientId: this.id,
+      fetchAllSchemas: false,
+      fetchAllVersions: false,
+      autoRegisterSchemas: true,
+      // autoRegisterSchemas: false,
+      wrapUnions: 'auto',
+      schemaFolder: `${process.cwd()}/src/schemas`,
+      produce: ['standard_cap', 'standard_geojson', TimeTopic],
+      logging: {
+        logToConsole: LogLevel.Info,
+        logToKafka: LogLevel.Warn,
+      },
+    });
+    this.adapter.on('error', e => console.error(e));
+    this.adapter.on('ready', () => {
+      log.info(`Current simulation time: ${this.adapter.trialTime}`);
+      log.info('Producer is connected');
+      this.sendCap();
+      // this.sendGeoJSON();
+      // this.sendTime();
+      // this.uploadFile();
+    });
+    this.adapter.connect();
+  }
+
+  // private sendGeoJSON() {
+  //   const geojson = geojsonToAvro((crowdTaskerMsg as unknown) as FeatureCollection);
+  //   const payloads: ProduceRequest[] = [
+  //     {
+  //       topic: 'standard_geojson',
+  //       messages: geojson,
+  //       attributes: 1, // Gzip
+  //     },
+  //   ];
+  //   this.adapter.send(payloads, (error, data) => {
+  //     if (error) {
+  //       log.error(error);
+  //     }
+  //     if (data) {
+  //       log.info(data);
+  //     }
+  //   });
+  // }
+
+  // private uploadFile() {
+  //   const file = path.resolve(process.cwd(), './dist/data/cap/examples/example_amber_alert.json');
+  //   const cb = largeFileUploadCallback(this.adapter, 'Amber alert message', 'This is a test message', DataType.json);
+  //   this.adapter.uploadFile(file, false, cb);
+  // }
+
+  // private sendTime() {
+  //   const d = new Date().valueOf();
+  //   const time = {
+  //     updatedAt: d,
+  //     timeElapsed: 0,
+  //     trialTimeSpeed: 1,
+  //     trialTime: d,
+  //     state: TimeState.Initialized,
+  //   } as ITiming;
+  //   const pr = { messages: time, topic: TimeTopic, attributes: 1 } as ProduceRequest;
+  //   this.adapter.send(pr, (err, data) => {
+  //     if (err) {
+  //       console.error(err);
+  //     } else {
+  //       console.info(data);
+  //     }
+  //   });
+  // }
+
+  /** Will only work if you are authorized to send CAP messages. */
+  private sendCap() {
+    const payloads: ProduceRequest[] = [
+      {
+        topic: 'standard_cap',
+        messages: amberAlert,
+        attributes: 1, // Gzip
+      },
+      {
+        topic: 'standard_cap',
+        messages: earthquakeAlert,
+        attributes: 1, // Gzip
+      },
+      {
+        topic: 'standard_cap',
+        messages: thunderstormAlert,
+        attributes: 1, // Gzip
+      },
+      {
+        topic: 'standard_cap',
+        messages: homelandSecurityAlert,
+        attributes: 1, // Gzip
+      },
+    ];
+    this.adapter.send(payloads, (error, data) => {
+      if (error) {
+        log.error(error);
+      }
+      if (data) {
+        log.info(data);
+      }
+    });
+  }
+}
+
+new Producer();
