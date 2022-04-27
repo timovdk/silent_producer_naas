7442fe62c55bcf40f7712f6a5d0979072b9f0f90
Erik Vullings <erik.vullings@gmail.com>
2022-04-27 18:55:19 +0200

    Wait SLEEP (10) seconds before uploading schema's

--- src/silent-producer.ts
+++ src/silent-producer.ts
@@ -1,23 +1,27 @@
-import { TestBedAdapter, Logger, LogLevel } from 'node-test-bed-adapter';
+import { Logger, LogLevel, TestBedAdapter } from 'node-test-bed-adapter';
 
 const log = Logger.instance;
 
-class SilentProducer {
-  private id = 'tno';
-  private adapter: TestBedAdapter;
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  constructor() {
-    this.adapter = new TestBedAdapter({
+const silentProducer = () => {
+  const id = 'tno-bootstrapper';
+
+  const initialize = async () => {
+    const waitFor = process.env.SLEEP || 10000;
+    console.log(`Waiting for ${+waitFor / 1000}s before uploading schemas.`);
+    await sleep(+waitFor);
+
+    const adapter = new TestBedAdapter({
       kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
       schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
-      // largeFileService: 'localhost:9090',
       // sslOptions: {
       //   pfx: fs.readFileSync('../certs/other-tool-1-client.p12'),
       //   passphrase: 'changeit',
       //   ca: fs.readFileSync('../certs/test-ca.pem'),
       //   rejectUnauthorized: true,
       // },
-      clientId: this.id,
+      clientId: id,
       fetchAllSchemas: false,
       fetchAllVersions: false,
       autoRegisterSchemas: true,
@@ -29,22 +33,23 @@ class SilentProducer {
         logToKafka: LogLevel.Warn,
       },
     });
-    this.adapter.on('error', (e) => console.error(e));
-    this.adapter.on('ready', async () => {
-      const createdTopics = await this.adapter.createTopics(
-        this.adapter.uploadedSchemas
-      );
+
+    adapter.on('error', (e) => console.error(e));
+    adapter.on('ready', async () => {
+      const createdTopics = await adapter.createTopics(adapter.uploadedSchemas);
       log.info(
         `Created the following topics:\n${createdTopics
           .sort()
           .map((t) => `- ${t}`)
           .join('\n')}\n`
       );
-      log.info('Exiting...');
+      log.info(`Exiting ${id}.`);
       process.exit(0);
     });
-    this.adapter.connect();
-  }
-}
+    adapter.connect();
+  };
 
-new SilentProducer();
+  initialize();
+};
+
+silentProducer();
