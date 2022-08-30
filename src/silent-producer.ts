07103ebee1ea6ebb8d7fe0848c4d14c716597a88
Timo van der Kuil <5330531+timovdk@users.noreply.github.com>
2022-08-30 20:46:11 +0200

    New env option support

--- src/silent-producer.ts
+++ src/silent-producer.ts
@@ -1,4 +1,9 @@
-import { Logger, LogLevel, TestBedAdapter } from 'node-test-bed-adapter';
+import {
+  CreateTopicRequest,
+  Logger,
+  LogLevel,
+  TestBedAdapter,
+} from 'node-test-bed-adapter';
 
 const log = Logger.instance;
 
@@ -15,12 +20,6 @@ const silentProducer = () => {
     const adapter = new TestBedAdapter({
       kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
       schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
-      // sslOptions: {
-      //   pfx: fs.readFileSync('../certs/other-tool-1-client.p12'),
-      //   passphrase: 'changeit',
-      //   ca: fs.readFileSync('../certs/test-ca.pem'),
-      //   rejectUnauthorized: true,
-      // },
       clientId: id,
       fetchAllSchemas: false,
       fetchAllVersions: false,
@@ -36,13 +35,54 @@ const silentProducer = () => {
 
     adapter.on('error', (e) => console.error(e));
     adapter.on('ready', async () => {
-      const createdTopics = await adapter.createTopics(adapter.uploadedSchemas);
-      log.info(
-        `Created the following topics:\n${createdTopics
-          .sort()
-          .map((t) => `- ${t}`)
-          .join('\n')}\n`
-      );
+      // Split the partition specification field
+      const partitionSpecification =
+        process.env.PARTITION_SPECIFICATION?.split(',') || [];
+
+      // Create a record of <topic, partitions>
+      let topicWithPartition = {} as Record<string, number>;
+      partitionSpecification.forEach((item: string) => {
+        const topic_with_partition = item.split(':');
+        topicWithPartition[topic_with_partition[0] as string] = Number(
+          topic_with_partition[1]
+        ) as number;
+      });
+
+      let schemas = adapter.uploadedSchemas;
+      const schemasToSend = schemas.map((topic: string) => {
+        // If the topic is in the specified list, use that no. of partitions
+        if (topic in topicWithPartition) {
+          return {
+            topic: topic,
+            partitions: topicWithPartition[topic]
+              ? topicWithPartition[topic]
+              : process.env.DEFAULT_PARTITIONS || 1,
+            replicationFactor: 1,
+          };
+        }
+        // else use the default partition field
+        return {
+          topic: topic,
+          partitions: process.env.DEFAULT_PARTITIONS || 1,
+          replicationFactor: 1,
+        };
+      }) as Array<CreateTopicRequest>;
+      try {
+        const createdTopics = await adapter.createTopics(schemasToSend);
+        if (createdTopics.length === 0) {
+          // Crash if the topics were not correctly created. This will trigger a restart which should resolve the issue.
+          console.log('0 topics created, restarting');
+          process.exit(1);
+        }
+        log.info(
+          `Created the following topics:\n${createdTopics
+            .sort()
+            .map((t) => `- ${t}`)
+            .join('\n')}\n`
+        );
+      } catch (error) {
+        console.log(error);
+      }
       log.info(`Exiting ${id}.`);
       process.exit(0);
     });
